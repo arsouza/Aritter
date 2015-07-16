b593b79ef266ccbf3335110533975f67cd5c54dc
Anderson Ritter de Souza <anderdsouza@gmail.com>
2015-07-15 23:26:06 -0300

    refact api project

--- src/Aritter.Manager.Api/Startup.cs
+++ src/Aritter.Manager.Api/Startup.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Aritter.Manager.Api.Migrations;
+using Aritter.Manager.Api.Models;
 using Microsoft.Owin;
 using Owin;
+using System.Data.Entity;
 
 [assembly: OwinStartup(typeof(Aritter.Manager.Api.Startup))]
 
 namespace Aritter.Manager.Api
 {
-    public partial class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            ConfigureAuth(app);
-        }
-    }
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			ConfigureAuth(app);
+			Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+		}
+	}
 }
