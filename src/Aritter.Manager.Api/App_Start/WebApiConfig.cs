71cc110946bad88e4383b87fa2e7d2e81f2a0388
Anderson Ritter de Souza <anderdsouza@gmail.com>
2015-07-20 10:15:34 -0300

    fixes api issues

--- src/Aritter.Manager.Api/App_Start/WebApiConfig.cs
+++ src/Aritter.Manager.Api/App_Start/WebApiConfig.cs
@@ -5,22 +5,23 @@ using System.Web.Http;
 
 namespace Aritter.Manager.Api
 {
-	public static class WebApiConfig
-	{
-		public static void Register(HttpConfiguration config)
-		{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
 
-			// Web API routes
-			config.MapHttpAttributeRoutes();
+            // Web API routes
+            config.MapHttpAttributeRoutes();
 
-			config.Routes.MapHttpRoute(
-				name: "DefaultApi",
-				routeTemplate: "api/{controller}/{id}",
-				defaults: new { id = RouteParameter.Optional }
-			);
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
 
-			var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
-			jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-		}
-	}
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.Indent = true;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
 }
