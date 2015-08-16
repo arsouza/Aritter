32238fd052d894b83da1eafd76a932c7c38a851d
Anderson Ritter de Souza <anderdsouza@gmail.com>
2015-08-15 22:24:11 -0300

    add new Aritter.API

---
+++ src/Aritter.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+namespace Aritter.API
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			config.SuppressDefaultHostAuthentication();
+
+			config.EnableCors();
+			// Web API routes
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			config.Formatters.Remove(config.Formatters.XmlFormatter);
+			config.Formatters.JsonFormatter.Indent = true;
+			config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+		}
+	}
+}
---
+++ src/Aritter.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aritter.API.WebApiApplication" Language="C#" %>
