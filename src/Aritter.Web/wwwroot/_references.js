7b8425b1017b5ddf16d224574ce7799df0461e83
Anderson Ritter de Souza <anderdsouza@gmail.com>
2015-09-04 12:54:22 -0300

    remove generate min files

--- src/Aritter.Web/wwwroot/_references.js
+++ src/Aritter.Web/wwwroot/_references.js
@@ -3,5 +3,4 @@
 /// <reference path="../gulpfile.js" />
 /// <reference path="app/app.module.js" />
 /// <reference path="app/app.routes.js" />
-/// <reference path="app/aritter.min.js" />
 /// <reference path="app/components/home/homecontroller.js" />
