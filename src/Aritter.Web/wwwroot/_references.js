65d74bf04de229d095e178c012a186b43767a4e1
Anderson Ritter de Souza <anderdsouza@gmail.com>
2015-09-09 00:27:48 -0300

    remove aritter.web

--- src/Aritter.Web/wwwroot/_references.js
+++
@@ -1,7 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../dist/app/js/aritter.min.js" />
-/// <reference path="../gruntfile.js" />
-/// <reference path="../gulpfile.js" />
-/// <reference path="app/app.module.js" />
-/// <reference path="app/app.routes.js" />
-/// <reference path="app/components/home/homecontroller.js" />
