bdd77c96d9717787711fcbec56e0e3d1dd7c8a8a
Anderson Ritter de Souza <anderdsouza@gmail.com>
2016-02-25 11:27:42 -0300

    refact directories

--- src/Aritter.API/Properties/AssemblyInfo.cs
+++ src/Aritter.API/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aritter.Presentation.WebApi")]
+[assembly: AssemblyTitle("Aritter.API")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Aritter.Presentation.WebApi")]
+[assembly: AssemblyProduct("Aritter.API")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
