a726c0004e9cbcae866936a4b64b1d9326cd0a8d
Anderson Ritter de Souza <andersonritter@cwi.com.br>
2016-11-08 10:04:55 -0200

    new features

--- src/Aritter.Infra.Data.Seedwork/Repository[].cs
+++ src/Aritter.Infra.Data.Seedwork/Repository[].cs
@@ -13,23 +13,13 @@ namespace Aritter.Infra.Data.Seedwork
 {
     public abstract class Repository<TEntity> : Repository, IRepository<TEntity> where TEntity : class, IEntity
     {
-        #region Properties
-
         public new IQueryableUnitOfWork UnitOfWork { get; private set; }
-
-        #endregion
-
-        #region Constructors
 
         protected Repository(IQueryableUnitOfWork unitOfWork)
             : base(unitOfWork)
         {
             UnitOfWork = unitOfWork;
         }
-
-        #endregion Constructors
-
-        #region Methods
 
         public virtual TEntity Get(int id)
         {
@@ -239,7 +229,5 @@ namespace Aritter.Infra.Data.Seedwork
                 UnitOfWork.Set<TEntity>().Add(entity);
             }
         }
-
-        #endregion Methods
     }
 }
