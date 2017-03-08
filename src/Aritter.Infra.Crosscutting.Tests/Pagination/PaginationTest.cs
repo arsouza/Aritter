e5e07188b041759b214b662e6591273fbfd08841
Anderson Ritter de Souza <anderdsouza@gmail.com>
2017-03-08 11:49:18 -0300

    add unit tests

---
+++ src/Aritter.Infra.Crosscutting.Tests/Pagination/PaginationTest.cs
@@ -0,0 +1,71 @@
+﻿using Aritter.Infra.Cosscutting.Extensions;
+using Aritter.Infra.Crosscutting.Pagination;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aritter.Infra.Crosscutting.Tests.Pagination
+{
+    [TestClass]
+    public class PaginationTest
+    {
+        private class TestObject
+        {
+            public int Id { get; set; }
+        }
+
+        [TestMethod]
+        public void Paginate()
+        {
+            IEnumerable<TestObject> values = GetTestObjectList();
+            Crosscutting.Pagination.Pagination pagination = new Crosscutting.Pagination.Pagination(0, 10);
+
+            var paginateResult = values.Paginate(pagination).ToList();
+
+            Assert.IsTrue(paginateResult.Count == 10);
+            Assert.IsTrue(paginateResult[0].Id == 1);
+        }
+
+        [TestMethod]
+        public void PaginateWithOrderAscending()
+        {
+            IEnumerable<TestObject> values = GetTestObjectList();
+            Crosscutting.Pagination.Pagination pagination = new Crosscutting.Pagination.Pagination(0, 10, "Id", true);
+
+            var paginateResult = values.Paginate(pagination).ToList();
+
+            Assert.IsTrue(paginateResult.Count == 10);
+            Assert.IsTrue(paginateResult[0].Id == 1);
+        }
+
+        [TestMethod]
+        public void PaginateWithOrderDescending()
+        {
+            IEnumerable<TestObject> values = GetTestObjectList();
+            Crosscutting.Pagination.Pagination pagination = new Crosscutting.Pagination.Pagination(0, 10, "Id", false);
+
+            var paginateResult = values.Paginate(pagination).ToList();
+
+            Assert.IsTrue(paginateResult.Count == 10);
+            Assert.IsTrue(paginateResult[0].Id == 100);
+        }
+
+        private IEnumerable<TestObject> GetTestObjectListWithLength(int length)
+        {
+            int id = 0;
+            List<TestObject> values = new List<TestObject>();
+
+            for (int i = 0; i < length; i++)
+            {
+                values.Add(new TestObject { Id = ++id });
+            }
+
+            return values;
+        }
+
+        private IEnumerable<TestObject> GetTestObjectList()
+        {
+            return GetTestObjectListWithLength(100);
+        }
+    }
+}
--- src/Aritter.Infra.Crosscutting/Extensions/Pagination.cs
+++ src/Aritter.Infra.Crosscutting/Extensions/Pagination.cs
@@ -7,48 +7,46 @@ using System.Threading.Tasks;
 
 namespace Aritter.Infra.Cosscutting.Extensions
 {
-	public static partial class ExtensionsMethods
-	{
-		public static IEnumerable<T> Paginate<T>(this IEnumerable<T> dataList, IPagination page)
-		{
-			return Paginate<T>(dataList.AsQueryable(), page);
-		}
+    public static partial class ExtensionsMethods
+    {
+        public static IEnumerable<T> Paginate<T>(this IEnumerable<T> dataList, IPagination page)
+        {
+            return Paginate<T>(dataList.AsQueryable(), page);
+        }
 
-		public static IQueryable<T> Paginate<T>(this IQueryable<T> dataList, IPagination page)
-		{
-			ValidatePagination(page);
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> dataList, IPagination page)
+        {
+            ValidatePagination(page);
 
-			var quaryableList = dataList;
+            var quaryableList = dataList;
 
-			if (!string.IsNullOrEmpty(page.OrderByName))
-				quaryableList = quaryableList.OrderBy(page.OrderByName, page.Ascending);
-			else
-				quaryableList = quaryableList.OrderBy(o => o);
+            if (!string.IsNullOrEmpty(page.OrderByName))
+                quaryableList = quaryableList.OrderBy(page.OrderByName, page.Ascending);
 
-			if (page.PageSize != 0)
-				quaryableList = quaryableList.Skip(page.PageIndex * page.PageSize);
+            if (page.PageSize != 0)
+                quaryableList = quaryableList.Skip(page.PageIndex * page.PageSize);
 
-			if (page.PageSize != 0)
-				quaryableList = quaryableList.Take(page.PageSize);
+            if (page.PageSize != 0)
+                quaryableList = quaryableList.Take(page.PageSize);
 
-			return quaryableList;
-		}
+            return quaryableList;
+        }
 
-		public static IPaginatedList<T> PaginateList<T>(this IEnumerable<T> dataList, IPagination page)
-		{
-			return PaginateList<T>(dataList.AsQueryable(), page);
-		}
+        public static IPaginatedList<T> PaginateList<T>(this IEnumerable<T> dataList, IPagination page)
+        {
+            return PaginateList<T>(dataList.AsQueryable(), page);
+        }
 
-		public static IPaginatedList<T> PaginateList<T>(this IQueryable<T> dataList, IPagination page)
-		{
-			return new PaginatedList<T>(dataList.Paginate<T>(page).ToList(), page, dataList.Count());
-		}
+        public static IPaginatedList<T> PaginateList<T>(this IQueryable<T> dataList, IPagination page)
+        {
+            return new PaginatedList<T>(dataList.Paginate<T>(page).ToList(), page, dataList.Count());
+        }
 
-		private static void ValidatePagination(IPagination page)
-		{
-			Check.Against<ArgumentNullException>(page == null, $"{nameof(page)} cannot be null.");
-			Check.Against<ArgumentNullException>(page.PageSize > 0, $"{nameof(page.PageSize)} must be greater then 0 (zero).");
-			Check.Against<ArgumentNullException>(page.PageIndex >= 0, $"{nameof(page.PageIndex)} must be greater then or equal to 0 (zero).");
-		}
-	}
+        private static void ValidatePagination(IPagination page)
+        {
+            Check.Against<ArgumentNullException>(page == null, $"{nameof(page)} cannot be null.");
+            Check.Against<ArgumentNullException>(page.PageSize <= 0, $"{nameof(page.PageSize)} must be greater then 0 (zero).");
+            Check.Against<ArgumentNullException>(page.PageIndex < 0, $"{nameof(page.PageIndex)} must be greater then or equal to 0 (zero).");
+        }
+    }
 }
