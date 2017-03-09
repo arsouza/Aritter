06e921db5ca8a04fa880306a9d4c86052aa4a32e
Anderson Ritter de Souza <anderdsouza@gmail.com>
2017-03-09 15:15:18 -0300

    update coverage script

--- src/Aritter.Infra.Crosscutting.Tests/coverage.bat
+++ src/Aritter.Infra.Crosscutting.Tests/coverage.bat
@@ -5,15 +5,20 @@ SET opencover=C:\Users\andersonritter\.nuget\packages\OpenCover\4.6.519\tools\Op
 SET reportgenerator=C:\Users\andersonritter\.nuget\packages\ReportGenerator\2.5.5\tools\ReportGenerator.exe
 
 SET targetargs="test"  
-SET filter="+[*]* -[*]*.Tests"  
+SET filter="+[Aritter*]* -[*Tests]*"  
 SET coveragefile=Coverage.xml  
 SET coveragedir=Coverage
 
+@echo on
+
 REM Run code coverage analysis  
-%opencover% -oldStyle -register:user -target:%dotnet% -output:%coveragefile% -targetargs:%targetargs% -filter:%filter% -skipautoprops -hideskipped:All
+%opencover% -oldStyle -register:user -target:%dotnet% -output:%coveragefile% -targetargs:%targetargs% -filter:%filter% -skipautoprops -hideskipped:All -log:All
 
 REM Generate the report  
 %reportgenerator% -targetdir:%coveragedir% -reporttypes:Html;Badges -reports:%coveragefile% -verbosity:Error
 
 REM Open the report  
 start "report" "%coveragedir%\index.htm"
+
+@echo off
+PAUSE
