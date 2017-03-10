abc4194642a6a0b0a10d5653062be3716b42ff07
Anderson Ritter de Souza <anderdsouza@gmail.com>
2017-03-10 12:08:11 -0300

    Check unit tests

--- src/Aritter.Infra.Crosscutting.Tests/Exceptions/CheckTest.cs
+++ src/Aritter.Infra.Crosscutting.Tests/Exceptions/CheckTest.cs
@@ -47,22 +47,144 @@ namespace Aritter.Infra.Crosscutting.Tests.Exceptions
             Assert.AreEqual("Message", exception.Message);
         }
 
-        public void CallInheritsFromShouldNotThrowAnException()
+        [TestMethod]
+        public void CallIsTypeOfShouldNotThrowAnException()
         {
-            Check.InheritsFrom<int>(1, "Message");
+            Check.IsTypeOf<String>("test", "Message");
         }
 
         [TestMethod]
-        public void CallInheritsFromShouldThrowAnException()
+        public void CallIsTypeOfShouldThrowAnException()
         {
             InvalidOperationException exception = Assert.ThrowsException<InvalidOperationException>(() =>
             {
-                Check.InheritsFrom<string>(1, "Message");
+                Check.IsTypeOf<String>(1, "Message");
             });
 
             Assert.IsNotNull(exception);
-            Assert.IsInstanceOfType(exception, typeof(InvalidOperationException));
+            Assert.IsInstanceOfType(exception, typeof(Exception));
             Assert.AreEqual("Message", exception.Message);
+        }
+
+        [TestMethod]
+        public void CallAreEqualsShouldNotThrowAnException()
+        {
+            Check.AreEquals<Exception>("test", "test", "Message");
+        }
+
+        [TestMethod]
+        public void CallAreEqualsShouldThrowAnException()
+        {
+            Exception exception = Assert.ThrowsException<Exception>(() =>
+            {
+                Check.AreEquals<Exception>("test", "no-test", "Message");
+            });
+
+            Assert.IsNotNull(exception);
+            Assert.IsInstanceOfType(exception, typeof(Exception));
+            Assert.AreEqual("Message", exception.Message);
+        }
+
+        [TestMethod]
+        public void CallIsNullShouldNotThrowAnException()
+        {
+            Check.IsNull(null, "Message");
+        }
+
+        [TestMethod]
+        public void CallIsNullShouldThrowAnException()
+        {
+            ArgumentNullException exception = Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                Check.IsNull("test", "value");
+            });
+
+            Assert.IsNotNull(exception);
+            Assert.IsInstanceOfType(exception, typeof(ArgumentNullException));
+            Assert.AreEqual("value", (exception as ArgumentNullException).ParamName);
+        }
+
+        [TestMethod]
+        public void CallIsNotNullShouldNotThrowAnException()
+        {
+            Check.IsNotNull("Test", "Message");
+        }
+
+        [TestMethod]
+        public void CallIsNotNullShouldThrowAnException()
+        {
+            ArgumentException exception = Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Check.IsNotNull(null, "value");
+            });
+
+            Assert.IsNotNull(exception);
+            Assert.IsInstanceOfType(exception, typeof(ArgumentException));
+            Assert.AreEqual("value", exception.ParamName);
+        }
+
+        [TestMethod]
+        public void CallIsNotEmptyShouldNotThrowAnException()
+        {
+            Check.IsNotEmpty("Test", "Message");
+        }
+
+        [TestMethod]
+        public void CallIsNotEmptySendEmptyShouldThrowAnException()
+        {
+            ArgumentException exception = Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Check.IsNotEmpty("", "value");
+            });
+
+            Assert.IsNotNull(exception);
+            Assert.IsInstanceOfType(exception, typeof(ArgumentException));
+            Assert.AreEqual("value", exception.ParamName);
+        }
+
+        [TestMethod]
+        public void CallIsNotEmptySendNullShouldThrowAnException()
+        {
+            ArgumentNullException exception = Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                Check.IsNotEmpty(null, "value");
+            });
+
+            Assert.IsNotNull(exception);
+            Assert.IsInstanceOfType(exception, typeof(ArgumentNullException));
+            Assert.AreEqual("value", exception.ParamName);
+        }
+
+        [TestMethod]
+        public void CallIsNotEmptyEnumerableShouldNotThrowAnException()
+        {
+            Check.IsNotEmpty(new int[] { 1 }, "Message");
+        }
+
+        [TestMethod]
+        public void CallIsNotEmptyEnumerableSendEmptyShouldThrowAnException()
+        {
+            ArgumentException exception = Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Check.IsNotEmpty(new int[] { }, "value");
+            });
+
+            Assert.IsNotNull(exception);
+            Assert.IsInstanceOfType(exception, typeof(ArgumentException));
+            Assert.AreEqual("value", exception.ParamName);
+        }
+
+        [TestMethod]
+        public void CallIsNotEmptyEnumerableSendNullShouldThrowAnException()
+        {
+            ArgumentNullException exception = Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                Check.IsNotEmpty<int>(null, "value");
+            });
+
+            Assert.IsNotNull(exception);
+            Assert.IsInstanceOfType(exception, typeof(ArgumentNullException));
+            Assert.AreEqual("value", exception.ParamName);
         }
     }
 }
--- src/Aritter.Infra.Crosscutting/Exceptions/Check.cs
+++ src/Aritter.Infra.Crosscutting/Exceptions/Check.cs
@@ -19,83 +19,47 @@ namespace Aritter.Infra.Crosscutting.Exceptions
                 throw (TException)Activator.CreateInstance(typeof(TException), args);
         }
 
-        public static void InheritsFrom<TBase>(object instance, string message)
+        public static void IsTypeOf<TType>(object instance, string message) where TType : class
         {
-            InheritsFrom<TBase>(instance.GetType(), message);
-        }
-
-        public static void InheritsFrom<TBase>(Type type, string message)
-        {
-            if (type.GetTypeInfo().BaseType != typeof(TBase))
+            if ((instance as TType) == null)
                 throw new InvalidOperationException(message);
         }
 
-        public static void Implements<TInterface>(object instance, string message)
-        {
-            Implements<TInterface>(instance.GetType(), message);
-        }
-
-        public static void Implements<TInterface>(Type type, string message)
-        {
-            if (!typeof(TInterface).IsAssignableFrom(type))
-            {
-                throw new InvalidOperationException(message);
-            }
-        }
-
-        public static void TypeOf<TType>(object instance, string message)
-        {
-            if (!(instance is TType))
-            {
-                throw new InvalidOperationException(message);
-            }
-        }
-
-        public static void IsEqual<TException>(object compare, object instance, string message)
+        public static void AreEquals<TException>(object compare, object instance, string message)
             where TException : Exception
         {
-            if (compare != instance)
-            {
+            if (!Equals(compare, instance))
                 throw (TException)Activator.CreateInstance(typeof(TException), message);
-            }
         }
 
         public static void IsNotNull(object instance, string parameterName)
         {
             if (ReferenceEquals(instance, null))
-            {
-                throw new ArgumentNullException(parameterName);
-            }
+                throw new ArgumentException(null, parameterName);
         }
 
         public static void IsNull(object instance, string message)
         {
             if (!ReferenceEquals(instance, null))
-            {
-                throw new ArgumentException(message);
-            }
+                throw new ArgumentNullException(message);
         }
 
         public static void IsNotEmpty<T>(IEnumerable<T> value, string parameterName)
         {
+            if (ReferenceEquals(value, null))
+                throw new ArgumentNullException(parameterName);
+
             if (!value.Any())
-            {
-                throw new ArgumentException($"{nameof(parameterName)} is empty");
-            }
+                throw new ArgumentException(null, parameterName);
         }
 
         public static void IsNotEmpty(string value, string parameterName)
         {
             if (ReferenceEquals(value, null))
-            {
                 throw new ArgumentNullException(parameterName);
-            }
-            else if (value.Trim().Length == 0)
-            {
-                throw new ArgumentException(parameterName);
-            }
 
-            throw new InvalidOperationException($"{nameof(parameterName)} could not be empty");
+            if (value.Trim().Length == 0)
+                throw new ArgumentException(null, parameterName);
         }
     }
 }
