e2c751296c5b5afa3145659da8f7c9d48907cdd6
Anderson Ritter de Souza <anderdsouza@gmail.com>
2017-06-29 15:38:11 -0300

    refactor(repository): remove inconsistent methods

--- src/Aritter.Domain.Seedwork/IRepository[].cs
+++ src/Aritter.Domain.Seedwork/IRepository[].cs
@@ -6,26 +6,19 @@ using System.Linq.Expressions;
 
 namespace Aritter.Domain.Seedwork
 {
-	public interface IRepository<TEntity> : IRepository where TEntity : class, IEntity
+    public interface IRepository<TEntity> : IRepository where TEntity : class, IEntity
     {
         #region Methods
 
-        bool Any();
-        bool Any(ISpecification<TEntity> specification);
+        ICollection<TEntity> FindAll();
 
         ICollection<TEntity> Find(ISpecification<TEntity> specification);
 
         ICollection<TEntity> Find<TProperty>(ISpecification<TEntity> specification, Expression<Func<TEntity, TProperty>> orderByExpression, bool ascending);
 
-        ICollection<TEntity> GetAll();
-
         IPaginatedList<TEntity> Find(IPagination pagination);
 
         IPaginatedList<TEntity> Find(ISpecification<TEntity> specification, IPagination pagination);
-
-        IPaginatedList<TEntity> Find<TProperty>(IPagination pagination, Expression<Func<TEntity, TProperty>> orderByExpression, bool ascending);
-
-        IPaginatedList<TEntity> Find<TProperty>(ISpecification<TEntity> specification, IPagination pagination, Expression<Func<TEntity, TProperty>> orderByExpression, bool ascending);
 
         TEntity Get(int id);
 
--- src/Aritter.Infra.Data.Seedwork/Repository[].cs
+++ src/Aritter.Infra.Data.Seedwork/Repository[].cs
@@ -1,218 +1,177 @@
 ﻿using Aritter.Domain.Seedwork;
 using Aritter.Domain.Seedwork.Specs;
-using Aritter.Infra.Crosscutting.Pagination;
 using Aritter.Infra.Crosscutting.Exceptions;
+using Aritter.Infra.Crosscutting.Extensions;
+using Aritter.Infra.Crosscutting.Pagination;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using Aritter.Infra.Crosscutting.Extensions;
 
 namespace Aritter.Infra.Data.Seedwork
 {
     public abstract class Repository<TEntity> : Repository, IRepository<TEntity>
-		where TEntity : class, IEntity
-	{
-		public new IQueryableUnitOfWork UnitOfWork { get; private set; }
+        where TEntity : class, IEntity
+    {
+        public new IQueryableUnitOfWork UnitOfWork { get; private set; }
 
-		protected Repository(IQueryableUnitOfWork unitOfWork)
-			: base(unitOfWork)
-		{
-			Check.IsNotNull(unitOfWork, nameof(unitOfWork));
+        protected Repository(IQueryableUnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+            Check.IsNotNull(unitOfWork, nameof(unitOfWork));
 
-			UnitOfWork = unitOfWork;
-		}
+            UnitOfWork = unitOfWork;
+        }
 
-		public virtual TEntity Get(int id)
-		{
-			return UnitOfWork
-			   .Set<TEntity>()
-			   .AsNoTracking()
-			   .FirstOrDefault(p => p.Id == id);
-		}
+        public virtual TEntity Get(int id)
+        {
+            return UnitOfWork
+               .Set<TEntity>()
+               .AsNoTracking()
+               .FirstOrDefault(p => p.Id == id);
+        }
 
-		public virtual TEntity Get(ISpecification<TEntity> specification)
-		{
-			Check.IsNotNull(specification, nameof(specification));
+        public virtual TEntity Get(ISpecification<TEntity> specification)
+        {
+            Check.IsNotNull(specification, nameof(specification));
 
-			return UnitOfWork
-				.Set<TEntity>()
-				.AsNoTracking()
-				.Where(specification.SatisfiedBy())
-				.FirstOrDefault();
-		}
+            return UnitOfWork
+                .Set<TEntity>()
+                .AsNoTracking()
+                .Where(specification.SatisfiedBy())
+                .FirstOrDefault();
+        }
 
-		public virtual bool Any()
-		{
-			return UnitOfWork
-				.Set<TEntity>()
-				.AsNoTracking()
-				.Any();
-		}
+        public virtual ICollection<TEntity> FindAll()
+        {
+            return UnitOfWork
+                .Set<TEntity>()
+                .AsNoTracking()
+                .ToList();
+        }
 
-		public virtual bool Any(ISpecification<TEntity> specification)
-		{
-			Check.IsNotNull(specification, nameof(specification));
+        public virtual ICollection<TEntity> Find(ISpecification<TEntity> specification)
+        {
+            Check.IsNotNull(specification, nameof(specification));
 
-			return UnitOfWork
-				.Set<TEntity>()
-				.AsNoTracking()
-				.Any(specification.SatisfiedBy());
-		}
+            var query = FindInternal(specification);
 
-		public virtual ICollection<TEntity> GetAll()
-		{
-			return UnitOfWork
-				.Set<TEntity>()
-				.AsNoTracking()
-				.ToList();
-		}
+            return query
+                .ToList();
+        }
 
-		public virtual ICollection<TEntity> Find(ISpecification<TEntity> specification)
-		{
-			Check.IsNotNull(specification, nameof(specification));
+        public ICollection<TEntity> Find<TProperty>(ISpecification<TEntity> specification, Expression<Func<TEntity, TProperty>> orderByExpression, bool ascending)
+        {
+            Check.IsNotNull(specification, nameof(specification));
+            Check.IsNotNull(orderByExpression, nameof(orderByExpression));
 
-			var query = FindInternal(specification);
+            var query = FindInternal(specification);
 
-			return query
-				.ToList();
-		}
+            if (orderByExpression != null)
+            {
+                query = ascending
+                    ? query.OrderBy(orderByExpression)
+                    : query.OrderByDescending(orderByExpression);
+            }
 
-		public ICollection<TEntity> Find<TProperty>(ISpecification<TEntity> specification, Expression<Func<TEntity, TProperty>> orderByExpression, bool ascending)
-		{
-			Check.IsNotNull(specification, nameof(specification));
-			Check.IsNotNull(orderByExpression, nameof(orderByExpression));
+            return query.ToList();
+        }
 
-			var query = FindInternal(specification);
+        public virtual IPaginatedList<TEntity> Find(IPagination pagination)
+        {
+            return Find(new DirectSpecification<TEntity>(t => true), pagination);
+        }
 
-			if (orderByExpression != null)
-			{
-				query = ascending
-					? query.OrderBy(orderByExpression)
-					: query.OrderByDescending(orderByExpression);
-			}
+        public virtual IPaginatedList<TEntity> Find(ISpecification<TEntity> specification, IPagination pagination)
+        {
+            Check.IsNotNull(specification, nameof(specification));
 
-			return query.ToList();
-		}
+            var query = FindInternal(specification);
 
-		public virtual IPaginatedList<TEntity> Find(IPagination pagination)
-		{
-			return Find(new DirectSpecification<TEntity>(t => true), pagination);
-		}
+            var totalCount = query.Count();
 
-		public virtual IPaginatedList<TEntity> Find(ISpecification<TEntity> specification, IPagination pagination)
-		{
-			Check.IsNotNull(specification, nameof(specification));
+            return query.PaginateList(pagination);
+        }
 
-			var query = FindInternal(specification);
+        public virtual void Save(TEntity entity)
+        {
+            SaveInternal(entity);
+            ((DbContext)UnitOfWork).SaveChanges();
+        }
 
-			var totalCount = query.Count();
+        public virtual void Save(IEnumerable<TEntity> entities)
+        {
+            Check.IsNotNull(entities, nameof(entities));
 
-			return query.PaginateList(pagination);
-		}
+            foreach (var entity in entities)
+            {
+                SaveInternal(entity);
+            }
 
-		public IPaginatedList<TEntity> Find<TProperty>(IPagination pagination, Expression<Func<TEntity, TProperty>> orderByExpression, bool ascending)
-		{
-			Check.IsNotNull(orderByExpression, nameof(orderByExpression));
+            ((DbContext)UnitOfWork).SaveChanges();
+        }
 
-			return Find(new DirectSpecification<TEntity>(t => true), pagination, orderByExpression, ascending);
-		}
+        public void Remove(TEntity entity)
+        {
+            if (entity != null)
+            {
+                UnitOfWork.Attach(entity);
+                UnitOfWork.Set<TEntity>().Remove(entity);
+            }
 
-		public IPaginatedList<TEntity> Find<TProperty>(ISpecification<TEntity> specification, IPagination pagination, Expression<Func<TEntity, TProperty>> orderByExpression, bool ascending)
-		{
-			Check.IsNotNull(orderByExpression, nameof(orderByExpression));
+            ((DbContext)UnitOfWork).SaveChanges();
+        }
 
-			var query = FindInternal(specification);
+        public void Remove(IEnumerable<TEntity> entities)
+        {
+            if (entities != null)
+            {
+                UnitOfWork.Attach(entities);
+                UnitOfWork.Set<TEntity>().RemoveRange(entities);
+            }
 
-			if (orderByExpression != null)
-			{
-				query = ascending
-					? query.OrderBy(orderByExpression)
-					: query.OrderByDescending(orderByExpression);
-			}
+            ((DbContext)UnitOfWork).SaveChanges();
+        }
 
-			return query.PaginateList(pagination);
-		}
+        public virtual void Remove(ISpecification<TEntity> specification)
+        {
+            Check.IsNotNull(specification, nameof(specification));
 
-		public virtual void Save(TEntity entity)
-		{
-			SaveInternal(entity);
-			((DbContext)UnitOfWork).SaveChanges();
-		}
+            var entities = UnitOfWork
+                .Set<TEntity>()
+                .Where(specification.SatisfiedBy())
+                .ToList();
 
-		public virtual void Save(IEnumerable<TEntity> entities)
-		{
-			Check.IsNotNull(entities, nameof(entities));
+            UnitOfWork
+                .Set<TEntity>()
+                .RemoveRange(entities);
 
-			foreach (var entity in entities)
-			{
-				SaveInternal(entity);
-			}
+            ((DbContext)UnitOfWork).SaveChanges();
+        }
 
-			((DbContext)UnitOfWork).SaveChanges();
-		}
+        private IQueryable<TEntity> FindInternal(ISpecification<TEntity> specification)
+        {
+            Check.IsNotNull(specification, nameof(specification));
 
-		public void Remove(TEntity entity)
-		{
-			if (entity != null)
-			{
-				UnitOfWork.Attach(entity);
-				UnitOfWork.Set<TEntity>().Remove(entity);
-			}
+            return UnitOfWork
+                .Set<TEntity>()
+                .AsNoTracking()
+                .Where(specification.SatisfiedBy());
+        }
 
-			((DbContext)UnitOfWork).SaveChanges();
-		}
+        private void SaveInternal(TEntity entity)
+        {
+            Check.IsNotNull(entity, nameof(entity));
 
-		public void Remove(IEnumerable<TEntity> entities)
-		{
-			if (entities != null)
-			{
-				UnitOfWork.Attach(entities);
-				UnitOfWork.Set<TEntity>().RemoveRange(entities);
-			}
-
-			((DbContext)UnitOfWork).SaveChanges();
-		}
-
-		public virtual void Remove(ISpecification<TEntity> specification)
-		{
-			Check.IsNotNull(specification, nameof(specification));
-
-			var entities = UnitOfWork
-				.Set<TEntity>()
-				.Where(specification.SatisfiedBy())
-				.ToList();
-
-			UnitOfWork
-				.Set<TEntity>()
-				.RemoveRange(entities);
-
-			((DbContext)UnitOfWork).SaveChanges();
-		}
-
-		private IQueryable<TEntity> FindInternal(ISpecification<TEntity> specification)
-		{
-			Check.IsNotNull(specification, nameof(specification));
-
-			return UnitOfWork
-				.Set<TEntity>()
-				.AsNoTracking()
-				.Where(specification.SatisfiedBy());
-		}
-
-		private void SaveInternal(TEntity entity)
-		{
-			Check.IsNotNull(entity, nameof(entity));
-
-			if (UnitOfWork.Set<TEntity>().Any(p => p.Id == entity.Id))
-			{
-				UnitOfWork.Set<TEntity>().Update(entity);
-			}
-			else
-			{
-				UnitOfWork.Set<TEntity>().Add(entity);
-			}
-		}
-	}
+            if (UnitOfWork.Set<TEntity>().Any(p => p.Id == entity.Id))
+            {
+                UnitOfWork.Set<TEntity>().Update(entity);
+            }
+            else
+            {
+                UnitOfWork.Set<TEntity>().Add(entity);
+            }
+        }
+    }
 }
