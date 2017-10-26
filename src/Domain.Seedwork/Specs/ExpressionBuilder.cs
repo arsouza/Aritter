91b75984a013a93f4ec3d085bbd6b0913811fba9
Anderson Ritter de Souza <anderdsouza@gmail.com>
2017-10-26 17:10:21 -0200

    refact bad named methods

--- src/Domain.Seedwork/Specs/ExpressionBuilder.cs
+++ src/Domain.Seedwork/Specs/ExpressionBuilder.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -8,13 +8,16 @@ namespace Ritter.Domain.Seedwork.Specs
     {
         public static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
         {
-            // build parameter map (from parameters of second to parameters of first)
-            var map = first.Parameters.Select((f, i) => new { f, s = second.Parameters[i] }).ToDictionary(p => p.s, p => p.f);
+            var map = first.Parameters
+                .Select((parameterExpression, index) => new
+                {
+                    FirstParameter = parameterExpression,
+                    SecondParameter = second.Parameters[index]
+                })
+                .ToDictionary(p => p.SecondParameter, p => p.FirstParameter);
 
-            // replace parameters in the second lambda expression with parameters from the first
             var secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
 
-            // apply composition of lambda expression bodies to parameters from the first expression
             return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
         }
 
