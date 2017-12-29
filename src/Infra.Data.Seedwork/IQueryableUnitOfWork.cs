bc1fe5e727fd36e97560e8cbaf1fd75ef2091208
Anderson Ritter de Souza <anderdsouza@gmail.com>
2017-12-29 15:53:27 -0200

    refact unused methods

--- src/Infra.Data.Seedwork/IQueryableUnitOfWork.cs
+++ src/Infra.Data.Seedwork/IQueryableUnitOfWork.cs
@@ -7,8 +7,6 @@ namespace Ritter.Infra.Data.Seedwork
 {
     public interface IQueryableUnitOfWork : IUnitOfWork
     {
-        EntityEntry Entry(object entity);
-        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
         DbSet<TEntity> Set<TEntity>() where TEntity : class;
         Task<int> SaveChangesAsync();
         int SaveChanges();
