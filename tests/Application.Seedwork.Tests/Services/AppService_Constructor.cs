c93cfc922939be6a2503154b65280257b0246d2b
Anderson Ritter de Souza <anderdsouza@gmail.com>
2018-03-28 18:15:48 -0300

    tests: fix tests

--- tests/Application.Seedwork.Tests/Services/AppService_Constructor.cs
+++ tests/Application.Seedwork.Tests/Services/AppService_Constructor.cs
@@ -1,9 +1,11 @@
 using FluentAssertions;
+using Xunit;
 
 namespace Application.Seedwork.Tests.Services
 {
     public class AppService_Constructor
     {
+        [Fact]
         public void CreateAnInstanceOfAppService()
         {
             var appService = new TestAppService(null);
