5564a5c6dfd723f723de151b887300ae4576162a
Anderson Ritter de Souza <anderdsouza@gmail.com>
2018-04-20 16:31:56 -0300

    test: add new domain unit tests

---
+++ tests/Domain.Seedwork.Tests/Specifications/DirectSpecification_SatisfiedBy.cs
@@ -0,0 +1,26 @@
+using FluentAssertions;
+using Ritter.Domain.Seedwork.Specifications;
+using Ritter.Domain.Seedwork.Tests.Mocks;
+using Xunit;
+
+namespace Domain.Seedwork.Tests.Specifications
+{
+    public class DirectSpecification_SatisfiedBy
+    {
+        [Fact]
+        public void GivenTransientEntityThenSatisfySpecification()
+        {
+            Specification<EntityTest> spec1 = new DirectSpecification<EntityTest>(e => e.Id == 0);
+
+            spec1.IsSatisfiedBy(new EntityTest()).Should().BeTrue();
+        }
+
+        [Fact]
+        public void GivenNotTransientEntityThenNotSatisfySpecification()
+        {
+            Specification<EntityTest> spec1 = new DirectSpecification<EntityTest>(e => e.Id == 0);
+
+            spec1.IsSatisfiedBy(new EntityTest(1)).Should().BeFalse();
+        }
+    }
+}
---
+++ tests/Domain.Seedwork.Tests/Specifications/TrueSpecification_SatisfiedBy.cs
@@ -0,0 +1,17 @@
+using FluentAssertions;
+using Ritter.Domain.Seedwork.Specifications;
+using Ritter.Domain.Seedwork.Tests.Mocks;
+using Xunit;
+
+namespace Domain.Seedwork.Tests.Specifications
+{
+    public class TrueSpecification_SatisfiedBy
+    {
+        [Fact]
+        public void GivenTransientEntityThenSatisfySpecification()
+        {
+            Specification<EntityTest> spec1 = new TrueSpecification<EntityTest>();
+            spec1.IsSatisfiedBy(new EntityTest()).Should().BeTrue();
+        }
+    }
+}
