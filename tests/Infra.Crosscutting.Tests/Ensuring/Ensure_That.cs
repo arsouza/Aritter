e79fd665f146589e319d6dcf6d0c75b3146fa872
Anderson Ritter de Souza <anderdsouza@gmail.com>
2021-07-15 23:03:14 -0300

    Cover Ensure class

--- tests/Infra.Crosscutting.Tests/Ensuring/Ensure_That.cs
+++ tests/Infra.Crosscutting.Tests/Ensuring/Ensure_That.cs
@@ -1,5 +1,5 @@
-using FluentAssertions;
 using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Ritter.Infra.Crosscutting.Tests.Ensuring
@@ -31,6 +31,34 @@ namespace Ritter.Infra.Crosscutting.Tests.Ensuring
         public void EnsureGivenTrueAndNotEmptyMessage()
         {
             Action act = () => Ensure.That(true, "Test");
+            act.Should().NotThrow<Exception>();
+        }
+
+        [Fact]
+        public void ThrowExceptionGivenFalsePredicate()
+        {
+            Action act = () => Ensure.That(() => false);
+            act.Should().Throw<Exception>().And.Message.Should().Be("");
+        }
+
+        [Fact]
+        public void ThrowExceptionGivenFalsePredicateAndNotEmptyMessage()
+        {
+            Action act = () => Ensure.That(() => false, "Test");
+            act.Should().Throw<Exception>().And.Message.Should().Be("Test");
+        }
+
+        [Fact]
+        public void EnsureGivenTruePredicate()
+        {
+            Action act = () => Ensure.That(() => true);
+            act.Should().NotThrow<Exception>();
+        }
+
+        [Fact]
+        public void EnsureGivenTruePredicateAndNotEmptyMessage()
+        {
+            Action act = () => Ensure.That(() => true, "Test");
             act.Should().NotThrow<Exception>();
         }
     }
