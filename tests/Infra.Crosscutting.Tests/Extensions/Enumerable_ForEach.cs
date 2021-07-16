ec14e6d57ccdc9939320d94785d4ce843c3ae5d1
Anderson Ritter de Souza <anderdsouza@gmail.com>
2021-07-15 23:42:14 -0300

    Cover Enumerable extensions

--- tests/Infra.Crosscutting.Tests/Extensions/Enumerable_ForEach.cs
+++ tests/Infra.Crosscutting.Tests/Extensions/Enumerable_ForEach.cs
@@ -1,8 +1,8 @@
-using FluentAssertions;
-using Ritter.Infra.Crosscutting.Tests.Mocks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using FluentAssertions;
+using Ritter.Infra.Crosscutting.Tests.Mocks;
 using Xunit;
 
 namespace Ritter.Infra.Crosscutting.Tests.Extensions
@@ -43,6 +43,47 @@ namespace Ritter.Infra.Crosscutting.Tests.Extensions
             Action act = () =>
             {
                 IEnumerable<TestObject1> source = null;
+                source.ForEach(p => { });
+            };
+
+            act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("source");
+        }
+
+        [Fact]
+        public void NotThrowExceptionGivenNotEmptyObjectEnumerable()
+        {
+            IEnumerable<object> source = new List<TestObject1>
+            {
+                new TestObject1 { Id = 1 },
+                new TestObject1 { Id = 2 }
+            };
+
+            source.ForEach(p =>
+            {
+                var obj = (TestObject1)p;
+                obj.Value = obj.Id.ToString();
+            });
+
+            source.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+            source.ElementAt(0).As<TestObject1>().Value.Should().Be("1");
+            source.ElementAt(1).As<TestObject1>().Value.Should().Be("2");
+        }
+
+        [Fact]
+        public void NotThrowExceptionGivenEmptyObjectEnumerable()
+        {
+            IEnumerable<object> source = new List<TestObject1>();
+            source.ForEach(p => { });
+
+            source.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [Fact]
+        public void ThrowArgumentNullExceptionGivenNullObjectEnumerable()
+        {
+            Action act = () =>
+            {
+                IEnumerable<object> source = null;
                 source.ForEach(p => { });
             };
 
