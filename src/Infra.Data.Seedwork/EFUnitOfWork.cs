26b4e8138205cc7cac5b645ae0d9f3b0968ae049
Anderson Ritter de Souza <anderdsouza@gmail.com>
2021-07-16 22:07:28 -0300

    update packages

--- src/Infra.Data.Seedwork/EFUnitOfWork.cs
+++ src/Infra.Data.Seedwork/EFUnitOfWork.cs
@@ -14,20 +14,11 @@ namespace Ritter.Infra.Data
         {
         }
 
-        public int ExecuteCommand(string sqlCommand, params object[] parameters)
-        {
-            return Database.ExecuteSqlRaw(sqlCommand, parameters);
-        }
+        public virtual int ExecuteCommand(string sqlCommand, params object[] parameters) => Database.ExecuteSqlRaw(sqlCommand, parameters);
 
-        public async Task<int> ExecuteCommandAsync(string sqlCommand, params object[] parameters)
-        {
-            return await Database.ExecuteSqlRawAsync(sqlCommand, parameters);
-        }
+        public virtual async Task<int> ExecuteCommandAsync(string sqlCommand, params object[] parameters) => await Database.ExecuteSqlRawAsync(sqlCommand, parameters);
 
-        public async Task<int> ExecuteCommandAsync(string sqlCommand, IEnumerable<object> parameters, CancellationToken cancellationToken = default)
-        {
-            return await Database.ExecuteSqlRawAsync(sqlCommand, parameters, cancellationToken);
-        }
+        public virtual async Task<int> ExecuteCommandAsync(string sqlCommand, IEnumerable<object> parameters, CancellationToken cancellationToken = default) => await Database.ExecuteSqlRawAsync(sqlCommand, parameters, cancellationToken);
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
