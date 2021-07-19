173fc019d410ffb3351d62d47466ed7dcb04610a
Anderson Ritter de Souza <anderdsouza@gmail.com>
2021-07-19 18:38:34 -0300

    testes business rules

--- src/Domain.Seedwork/Business/BusinessRule.cs
+++ src/Domain.Seedwork/Business/BusinessRule.cs
@@ -1,16 +1,16 @@
+using System;
 using Ritter.Infra.Crosscutting;
 using Ritter.Infra.Crosscutting.Specifications;
-using System;
 
 namespace Ritter.Domain.Business
 {
-    public class BusinessRule<TEntity> : IBusinessRule<TEntity>
+    public abstract class BusinessRule<TEntity> : IBusinessRule<TEntity>
         where TEntity : class
     {
         private readonly ISpecification<TEntity> rule;
         private readonly Action<TEntity> action;
 
-        public BusinessRule(ISpecification<TEntity> rule, Action<TEntity> action)
+        protected BusinessRule(ISpecification<TEntity> rule, Action<TEntity> action)
         {
             Ensure.ArgumentNotNull(rule, nameof(rule), $"Please provide a valid non null {nameof(rule)} delegate instance.");
             Ensure.ArgumentNotNull(action, nameof(action), $"Please provide a valid non null {nameof(action)} delegate instance.");
@@ -24,7 +24,9 @@ namespace Ritter.Domain.Business
             Ensure.ArgumentNotNull(entity, nameof(entity), "Cannot evaulate a business rule against a null reference.");
 
             if (rule.IsSatisfiedBy(entity))
+            {
                 action(entity);
+            }
         }
     }
 }
